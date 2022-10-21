8594b607a44686e8ac40ce2b2bf6e5319110fc11
GutsAndBlood <ajgo_24@hotmail.com>
2022-10-21 03:37:54 -0500

    Adding git ignore and creating testcase01

--- src/test/java/demoshop/testcase/MainFuntionalityDemoShop.java
+++ src/test/java/demoshop/testcase/MainFuntionalityDemoShop.java
@@ -1,5 +1,37 @@
 package demoshop.testcase;
 
-public class MainFuntionalityDemoShop {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
+public class MainFuntionalityDemoShop {
+	
+	WebDriver driver;
+	String driverPath=System.getProperty("user.dir");
+	
+	@BeforeTest
+	public void StartUp() {
+		
+			  
+		System.setProperty("webdriver.chrome.driver", driverPath+"/drivers/chromedriver.exe");
+		driver = new ChromeDriver();		  
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		
+
+	}
+	
+	@Test
+	public void Testcase01() {
+		driver.get("https://www.google.com/"); 
+		
+	}
+	
+	@AfterTest
+	public void TearDown() {
+		driver.quit();
+	}
 }
